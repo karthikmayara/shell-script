a32fc6896a9c5b42afcb4a92fda1153b3577e13a
karthik <mayarakarthik299@gmail.com>
2025-04-07 11:35:54 +0530

    variables added

---
+++ 02-variables-arguments.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#if you want to add variables
+person1=rahim
+person2=mant
+
+#if you want to add arguments or args
+person1=$1
+person2=$2
+
+echo "rahim:: hi"
+echo "mant:: hi"
+echo "rahim: how are you"
+echo "mant: i am good"
+
